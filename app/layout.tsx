9036ebf3d2aee8a8720f4264642ff996574a500a
qcfq <kingramyar7@gmail.com>
2026-03-08 17:54:10 +0300

    smth dumb

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,29 @@
-import "./globals.css"
-import type { Metadata } from "next"
+import "./globals.css";
+import type { Metadata } from "next";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Ace Social",
-  description: "All my links in one place",
-}
+  description: "All my social links in one place",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <head>
-        <script
+      <body>
+        {children}
+        {/* Umami script */}
+        <Script
+          strategy="afterInteractive"
           async
           src="https://cloud.umami.is/script.js"
           data-website-id="92397e0e-8b6f-4887-a31b-805ae724402b"
-        ></script>
-      </head>
-      <body>{children}</body>
+        />
+      </body>
     </html>
-  )
+  );
 }
