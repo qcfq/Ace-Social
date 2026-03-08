4def24479461a062c059b3cd463254b845e97927
qcfq <kingramyar7@gmail.com>
2026-03-08 16:42:39 +0300

    initial commit

--- app/components/LinkCard.tsx
+++ app/components/LinkCard.tsx
@@ -7,11 +7,12 @@ type Props = {
 export default function LinkCard({ title, link, icon }: Props) {
   return (
     <a
-  href={link}
-  target="_blank"
-  onClick={() => console.log(title + " clicked")}
-  className="bg-neutral-900 hover:bg-neutral-800 hover:scale-105 transition p-4 rounded-xl flex items-center justify-center gap-3"
-></a>
-
+      href={link}
+      target="_blank"
+      className="bg-neutral-900 hover:bg-neutral-800 hover:scale-105 transition p-4 rounded-xl flex items-center justify-center gap-3"
+    >
+      {icon}
+      {title}
+    </a>
   )
 }
